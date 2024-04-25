5df4ea12f13f39c5842505489d084d7d2a297bd9
Ujawall <ujawalldwivedi@gmail.com>
2024-04-25 19:00:48 +0530

    States, Events and form interactive components

---
+++ 003-travel_list/src/App.js
@@ -0,0 +1,49 @@
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Towel", quantity: 1, packed: false },
+];
+export default function App() {
+
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
+
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form(){
+  return <div className='add-form'>
+    <h3> What do you need for your trip ? 😍</h3>
+  </div>;
+}
+
+function PackingList(){
+  return <div className='list'>
+    <ul>LIST
+      {initialItems.map(item => <Item key={item.id} item={item} />)}
+    </ul>
+  </div>;
+}
+
+function Item({ item }) {
+  return <li>
+    <span style={item.packed ? { textDecoration: "line-through" } : undefined}>
+      {item.description} {item.quantity}
+    </span>
+    <button>❌</button>
+  </li>;
+}
+
+function Stats(){
+  return <footer className='stats'>
+    🎒 <em>You have x items in your list and you have already packed x itmes (x%).</em>
+  </footer>
+}
